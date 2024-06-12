a306e52687a6055dbf7276de690635c25d462f2d
vladakolos <linikvladislava@gmail.com>
2024-06-12 23:28:49 +0800

    first deploy

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import Product from "../Products/Products";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product name="Tacos With Lime" />
+      <Product name="Fries and Burger" />
+    </div>
+  );
+}
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,24 @@
+// src/components/Product.jsx
+
+const Product = () => {
+  const price = 999;
+  const imgUrl =
+    "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+  );
+};
+export default Product;
